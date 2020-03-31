a73e8f392f86524a2290e79f93a8fba6cbd48ea2
Manya06 <juanma.otegui@gmail.com>
2020-03-31 11:37:40 -0300

    Creado clase FuncionesUsuario y calcularImpuesto

---
+++ src/FuncionesUsuario.java
@@ -0,0 +1,25 @@
+
+public class FuncionesUsuario {
+
+	// Calculo el impuesto a pagar
+	public double calcularImpuestos(Usuario usuario) {
+		// Si es menor no paga impuestos
+		if (usuario.getEdad() < 18) return 0;
+		
+		double impuesto = 0;
+		
+		// Calculo el impuesto segun la edad
+		if (usuario.getEdad() <= 30) 
+			impuesto = 2000;
+		else if (usuario.getEdad() <= 65) 
+			impuesto = 7000;
+		else impuesto = 2000;
+		
+		// Le descuento segun la cantidad de hijos
+		int descuento = usuario.getCantidadHijos() * 3;
+		if (descuento >= 50) descuento = 50;
+		
+		return impuesto * (1 - (descuento / 100));
+	}
+	
+}
